1974a1094b68b7180cf98b4e0a96a0421ea076b2
misha-advena <misha.advena@gmail.com>
2025-04-14 13:37:27 -0400

    updated with gh_stars variable name

--- 2_kestra/stars.py
+++ 2_kestra/stars.py
@@ -1,8 +1,8 @@
 import requests
 
 r = requests.get("https://api.github.com/repos/kestra-io/kestra")
-gh = r.json()['stargazers_count']
-print(f"Kestra has {gh} stars on GitHub.")
+gh_stars = r.json()['stargazers_count']
+print(f"Kestra has {gh_stars} stars on GitHub.")
 # The above code fetches the number of stars for the Kestra GitHub repository using the GitHub API.
 # It uses the requests library to make a GET request to the API endpoint for the repository.
 # The response is then parsed as JSON, and the number of stargazers (stars) is extracted and printed.
